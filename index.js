2bec9e801e880e15d1ea0978146e1b8bbece80de
MANISHAKOSGI <manishakosgi00@gmail.com>
2023-12-06 09:13:00 +0000

    congratscardapp

---
+++ index.js
@@ -0,0 +1,21 @@
+const element = (
+  <div className="congrats-card-container">
+    <h1 className="heading">Congratulations</h1>
+    <div className="card">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-profile-img.png"
+        className="image"
+      />
+      <h1 className="card-title">Kiran V</h1>
+      <p class="card-para">
+        vishnu Institude of computer and Technology.Bhimavaram
+      </p>
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/congrats-card-watch-img.png"
+        class="watch-img"
+      />
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
